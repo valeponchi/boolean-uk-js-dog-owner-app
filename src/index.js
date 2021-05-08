b950e635ba61b4cf63e9764a1e025df57702c70d
valeponchi <81304855+valeponchi@users.noreply.github.com>
2021-05-08 17:30:36 +0100

    yes/no & button changing

--- src/index.js
+++ src/index.js
@@ -1,93 +1,76 @@
 console.log(data);
-// We create individual components here
+
 
 let dogListUlEl = document.querySelector(`.dogs-list`)
-let main = document.querySelector(`main`)
-let noDogsQ = document.querySelector(`.main__dog-section`)
-let ulEl = document.querySelector(`.dogs-list`) 
+let mainDogSection = document.querySelector(`.main__dog-section`) //WRAPPER
+let addDogListEl = document.querySelector(".dogs-list__button--add");
 
 
-// FUNCTIONS:
-// 1.createDogButton
-// 2.cardsWhenClickBtn
-// 3.creationOfDogCard
-
-function createDogButton() {
-  for (i=0; i<data.length; i++) {
-
-    let dogButton = document.createElement(`li`)
-    dogButton.innerText = data[i].name
-    dogButton.setAttribute(`class`, `dogs-list__button`)
-    dogListUlEl.append(dogButton)
-    
-    function cardsWhenClickBtn() {
-      for (const li of ulEL) {
-        dogButton.addEventListener(`click`, function(event) {
-          creationOfDogCard()
-          
-          event.stopPropagation() //stop this event to happen to each of this El's parent
-      }) } }
+function createDogButtons(array) {
+  for (i=0; i<array.length; i++) {
+    createDogButton(array[i])
   }
 }
 
+function createDogButton(dog){
 
-
-function creationOfDogCard() {
-  noDogsQ.remove()
-  let mainDogSection = document.createElement(`section`)     //SECTION WRAPPER OF THE CARD
-  mainDogSection.setAttribute(`class`, `main__dog-section`)  //CLASS
-  main.append(mainDogSection)                       //WHERE IS IT ON THE PAGE
+  let dogButton = document.createElement(`li`)
+  dogButton.innerText = dog.name
+  dogButton.setAttribute(`class`, `dogs-list__button`)
+  dogListUlEl.append(dogButton)
   
+      dogButton.addEventListener(`click`, function() {
+        
+        mainDogSection.innerHTML = ""
+        creationOfDogCard(dog)
+    }) 
+}
+
+function creationOfDogCard(dog) {
+  mainDogSection.innerHTML = "";
+
     let cardName = document.createElement(`h2`)        //SECT-CHILD = TITLE
-    cardName.innerText = data[0].name
-    mainDogSection.append(cardName)
+    cardName.innerText = dog.name
   
     let imgCard = document.createElement(`img`)        //SECT-CHILD = IMG
-    imgCard.setAttribute(`src`, data[0].image)
-    mainDogSection.append(imgCard)
+    imgCard.setAttribute(`src`, dog.image)
+    imgCard.setAttribute(`alt`, dog.name);
+    imgCard.setAttribute(`height`, 300);
   
     let bioSection = document.createElement(`div`)     //SECT-CHILD = BIO DIV
     bioSection.setAttribute(`class`, `main__dog-section__desc`)
-    mainDogSection.append(bioSection)
     
         let bioTitle = document.createElement(`h3`)    // BIO-CHILD
-        bioSection.append(bioTitle)
         bioTitle.innerText = `Bio`
         
         let bioText = document.createElement(`p`)      //BIO-CHILD
-        bioSection.append(bioText)
-        bioText.innerText = data[0].bio
+        bioText.innerText = dog.bio
+
+        bioSection.append(bioTitle, bioText)
      
-    let behaviourSection = document.createElement(`div`)    //SECT-CHILD = BEHAVIOUR DIV
-    behaviourSection.setAttribute(`class`, `main__dog-section__btn`)
-    mainDogSection.append(behaviourSection)
+    let naughtyLine = document.createElement("p")
+
+        let naughtyQ = document.createElement("em")
+        naughtyQ.innerText = "Is naughty?"
+
+        let isDogNaughty = !dog.isGoodDog
+        naughtyLine.append(naughtyQ, isDogNaughty ? " yes!" : " no!");
   
-        let naughtyGoodQ = document.createElement(`p`)      //BEHAVIOUR-CHILD
-        behaviourSection.append(naughtyGoodQ)
-        naughtyGoodQ.setAttribute(`class`, `em`) //I added stuff in CSS file
-        naughtyGoodQ.innerText = `Is naughty?`
-  
-        let behaviourText = document.createElement(`p`)     //BEHAVIOUR-CHILD
-        behaviourSection.append(behaviourText)
-        behaviourText.innerText = `No!`
-  
-        let behaviourButton = document.createElement(`button`) //BEHAVIOUR-CHILD
-        behaviourSection.append(behaviourButton)
-        behaviourButton.innerText = `Bad Dog`
+    let behaviourButton = document.createElement(`button`) //BEHAVIOUR-CHILD
+    behaviourButton.innerText = isDogNaughty ? "Good dog!" : "Bad dog!";
+    
+    behaviourButton.addEventListener("click", function () {
+      // toggle the dog's goodness
+      dog.isGoodDog = !dog.isGoodDog;
+      
+      // render the dog
+      creationOfDogCard(dog);
+    });
+    
+    mainDogSection.append(cardName, imgCard, bioSection, naughtyLine, behaviourButton)
   }
  
 
 
-// CALLING FUNCTIONS:
-createDogButton()
-// cardsWhenClickBtn()
-//QUESTION:
-// creationOfDogCard() //this is called inside the buttons?
+createDogButtons(data)
 
-
-
-
-
-//remember:
-// event.preventDefault() //prevent the Default refresh of the Form
-// event.reset() //clear all the inputs
